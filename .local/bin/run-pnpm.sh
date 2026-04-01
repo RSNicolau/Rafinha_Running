66534ec89937274aa07942231988c15b49264383
Naboah Tech <rodrigonicolau1981@gmail.com>
2026-04-01 10:55:52 -0300

    Fix: exclude .local binaries from git

--- .local/bin/run-pnpm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Wrapper to run pnpm with correct PATH
-DIR="$(cd "$(dirname "$0")" && pwd)"
-export PATH="$DIR:$DIR/.tools/pnpm-exe/10.30.3:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
-exec "$DIR/pnpm" "$@"
